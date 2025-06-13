f605eeca873d07d9edcce5f088393d7345329733
anshulsc <anshulsinghchambial@gmail.com>
2025-06-13 20:30:10 +0530

    Sequence

---
+++ tinyvllm/config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from transformers import AutoConfig
+
+@dataclass
+class Config:
+    model: str = ''
+    max_num_batched_tokens: int = 32768
+    max_num_seqs: int = 512
+    max_model_len: int = 4096
+    gpu_memory_utilization: float = 0.9
+    enforce_eager: bool = False
+    hf_config: AutoConfig | None = None
+    eos: int = -1
+    kvcache_block_size: int = 256
+    num_kvcache_blocks: int = -1
